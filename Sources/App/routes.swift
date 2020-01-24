2d8d3fde2387b485c2105ea0402311da277c7a5b
aiQG_ <lovequange1314@vip.qq.com>
2020-01-24 13:16:16 +0800

    print get requests

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "It works!\(req)"
     }
     
     // Basic "Hello, world!" example
