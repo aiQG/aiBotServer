6524101d9cf710f5460144c914af797757d8a4fa
aiQG_ <lovequange1314@vip.qq.com>
2020-01-24 13:32:03 +0800

    post test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,8 +3,7 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-	router.get { req -> String in
-		print(req)
+	router.get { req in
         return "It works!\(req)"
     }
     
@@ -13,6 +12,11 @@ public func routes(_ router: Router) throws {
         return "Hello, world!\(req)"
     }
 
+	router.post { req -> String in
+		print(req)
+		return "\(req)"
+	}
+	
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
