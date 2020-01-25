d19522d2592a241c6e7a2a5b7543aa38b51f03f9
aiQG_ <lovequange1314@vip.qq.com>
2020-01-25 12:02:43 +0800

    JSON try fix error type

--- Sources/App/Models/JSONMessage.swift
+++ Sources/App/Models/JSONMessage.swift
@@ -8,23 +8,23 @@
 import Vapor
 
 struct JSONMessage: Content {
-	var font: uint64
+	var font: UInt64
 	var message: String
-	var message_id: uint32
+	var message_id: UInt32
 	var message_type: String
 	var post_type: String
 	var raw_message: String
-	var self_id: uint64
+	var self_id: UInt64
 	struct JSONSender: Content {
-		var age: uint32
+		var age: UInt32
 		var nickname: String
 		var sex: String
-		var user_id: uint64
+		var user_id: UInt64
 	}
 	var sender: JSONSender
 	var sub_type: String
-	var time: uint64
-	var user_id: uint64
+	var time: UInt64
+	var user_id: UInt64
 	
 	
 }
