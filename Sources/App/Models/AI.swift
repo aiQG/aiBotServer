d23ed408ba8e1f0c00cef366010e5c8aafc37ff1
aiQG_ <lovequange1314@vip.qq.com>
2020-02-07 16:34:14 +0800

    debug

--- Sources/App/Models/AI.swift
+++ Sources/App/Models/AI.swift
@@ -105,7 +105,7 @@ class AI {
 			return
 			
 		case "fortune":
-			self.replyMessage.reply = "\n" + execCmds(arg: ["fortune"])
+			self.replyMessage.reply =  execCmds(arg: ["fortune"])
 			return
 			
 			// 等待测试环境
