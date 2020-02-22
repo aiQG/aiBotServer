4274dccebe28fc198b04289e2e878e8b5425e6a9
aiQG_ <lovequange1314@vip.qq.com>
2020-02-22 18:53:18 +0800

    test

--- Sources/App/Models/AI.swift
+++ Sources/App/Models/AI.swift
@@ -122,7 +122,7 @@ class AI {
 	}
 	
 	func privateMessage() {
-		print(message.message)
+		print(message.message) 
 		// 判断色图
 		let CQImageRange = message.message!
 			.range(of: "\\[CQ:image,file=[A-F0-9]*(\\.jpg|\\.png),url=(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]\\]",
