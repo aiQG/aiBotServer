1be842ee26ea7693d6db726444e158c9eb3781dd
aiQG_ <lovequange1314@vip.qq.com>
2020-02-26 13:36:52 +0800

    debug

--- Sources/App/Models/AI.swift
+++ Sources/App/Models/AI.swift
@@ -140,6 +140,7 @@ class AI {
 	}
 	
 	func privateMessage() {
+		print(TimeZone.current.identifier)
 		print(Calendar.current.dateComponents([.year,.month, .day, .hour,.minute,.second,.weekday], from: Date()).hour, Calendar.current.dateComponents([.year,.month, .day, .hour,.minute,.second,.weekday], from: Date()).weekday)
 		print(message.message)
 		// 判断色图
