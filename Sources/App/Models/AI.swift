81acb5aa31cbe26cfe7ed3e6085e38da2271c283
aiQG_ <lovequange1314@vip.qq.com>
2020-05-29 14:52:06 +0800

    尝试修复

--- Sources/App/Models/AI.swift
+++ Sources/App/Models/AI.swift
@@ -229,7 +229,7 @@ final class AI {
 				}
 			case "w":
 				do {
-					SeTuURLs = SeTuURLs.fliter {
+					SeTuURLs = SeTuURLs.filter {
 						execCmds("curl \($0)") != ""
 					}
 					let text = SeTuURLs.joined(separator: "\n")
